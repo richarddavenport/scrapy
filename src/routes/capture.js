e5213431030ae14cbbf8adbdcd7341f0560c7728
Richard Davenport <richard.davenport@mbpnetwork.com>
2021-12-06 09:34:34 -0600

    Change to regular import

--- src/routes/capture.js
+++ src/routes/capture.js
@@ -1,13 +1,13 @@
-export const get = async ({ query }) => {
-  const { chromium } = await import('playwright-core');
+import * as playwright from 'playwright-core';
 
+export const get = async ({ query }) => {
   const queryUrl = query.get('url');
   const queryFullscreen = query.get('fullscreen');
   const queryWidth = query.get('width');
   const queryHeight = query.get('height');
 
   const url = new URL(queryUrl);
-  const browser = await chromium.launch();
+  const browser = await playwright.chromium.launch();
   const page = await browser.newPage();
   await page.setViewportSize({ width: +queryWidth, height: +queryHeight });
   await page.goto(url.toString());
